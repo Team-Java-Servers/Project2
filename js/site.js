a81bb9ca607b54e90b25fb305ef6849d1f94a8ba
Kiara McKenzie <60056749+KiaraMcKenzie@users.noreply.github.com>
2020-11-11 13:34:23 -0600

    everything added

---
+++ js/site.js
@@ -0,0 +1,50 @@
+// jQuery 3.x-style ready event and locally scoped $
+$(document).ready(function() {
+
+  jQuery(function($) {
+    $('html').removeClass('nojs');
+    $('html').addClass('hasjs');
+  });
+
+  $('#burger').click(function() {
+    $('#menu').toggle('#menu');
+    $('#container').toggle('#container.active');
+  })
+
+  $('#history').click(function() {
+    $('.history').toggle('.history');
+    if($('.constitution').is(':visible'))
+    {
+      $('.constitution').toggle('.constitution');
+    }
+    if($('.journal').is(':visible'))
+    {
+      $('.journal').toggle('.journal');
+    }
+  })
+
+  $('#constitution').click(function() {
+    $('.constitution').toggle('.constitution');
+    if($('.history').is(':visible'))
+    {
+      $('.history').toggle('.history');
+    }
+    if($('.journal').is(':visible'))
+    {
+      $('.journal').toggle('.journal');
+    }
+  })
+
+  $('#journal').click(function() {
+    $('.journal').toggle('.journal');
+    if($('.history').is(':visible'))
+    {
+      $('.history').toggle('.history');
+    }
+    if($('.constitution').is(':visible'))
+    {
+      $('.constitution').toggle('.constitution');
+    }
+  })
+
+})
